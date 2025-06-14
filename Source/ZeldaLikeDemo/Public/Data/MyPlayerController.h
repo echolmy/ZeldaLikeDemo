c79f4332af616139d45d247223f1f932b9360e13
echolmy <echoliumy@outlook.com>
2025-06-14 03:32:31 +0100

    create basic cpp files about game mode

---
+++ Source/ZeldaLikeDemo/Public/Data/MyPlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MyPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZELDALIKEDEMO_API AMyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
