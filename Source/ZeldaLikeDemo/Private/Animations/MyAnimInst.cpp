27c0e32a56a93914e0eee9a83b5ca0bade8485a5
echolmy <echoliumy@outlook.com>
2025-06-16 00:55:49 +0100

    implement custom basic locomotion

---
+++ Source/ZeldaLikeDemo/Private/Animations/MyAnimInst.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animations/MyAnimInst.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UMyAnimInst::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	PlayerRef = Cast<AMyCharacterBase>(TryGetPawnOwner());
+	if (!PlayerRef) return;
+	MoveComp = PlayerRef->GetCharacterMovement();
+}
+
+void UMyAnimInst::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	if (!PlayerRef || !MoveComp) return;
+
+	GroundSpeed = UKismetMathLibrary::VSizeXY(PlayerRef->GetVelocity());
+	AirSpeed = PlayerRef->GetVelocity().Z;
+	bIsFalling = MoveComp->IsFalling();
+	bShouldMove = !bIsFalling && GroundSpeed > 5.0f && MoveComp->GetCurrentAcceleration().Size() > 0;
+	bIsGliding = PlayerRef->CurrentMT == EMovementTypes::MM_GLIDING;
+	bReadyToThrow = PlayerRef->bReadyToThrow;
+}
---
+++ Source/ZeldaLikeDemo/Public/Animations/MyAnimInst.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "Characters/MyCharacterBase.h"
+#include "MyAnimInst.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZELDALIKEDEMO_API UMyAnimInst : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "References")
+	AMyCharacterBase* PlayerRef;
+
+	UPROPERTY()
+	UCharacterMovementComponent* MoveComp;
+
+	UPROPERTY(visibleanywhere, BlueprintReadOnly, Category = "References")
+	float GroundSpeed = 0.0f;
+
+	UPROPERTY(visibleanywhere, BlueprintReadOnly, Category = "References")
+	float AirSpeed = 0.0f;
+
+	UPROPERTY(visibleanywhere, BlueprintReadOnly, Category = "References")
+	bool bShouldMove = false;
+
+	UPROPERTY(visibleanywhere, BlueprintReadOnly, Category = "References")
+	bool bIsFalling = false;
+
+	UPROPERTY(visibleanywhere, BlueprintReadOnly, Category = "References")
+	bool bIsGliding = false;
+
+	UPROPERTY(visibleanywhere, BlueprintReadOnly, Category = "References")
+	bool bReadyToThrow = false;
+	
+	
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+};
